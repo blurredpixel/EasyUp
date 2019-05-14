7d1918d1e872c19a1a2c4d55cdd1f8c547106050
David J. Fries <friesdj@greenfiber.com>
2019-05-14 13:41:53 -0400

    upload page start, updated gitignore, index page start

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for,render_template
 import flask-uploads
+
+@route('/',methods = ['GET'])
+def default():
+    return render_template('index.html')
