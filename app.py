0d5310711e033688f5485ca89cd0753a74413337
David J. Fries <friesdj@greenfiber.com>
2019-05-14 15:39:56 -0400

    gen file id, start upload handling from form

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, flash, request, redirect, url_for,render_template
 from flask_uploads import UploadSet, IMAGES, configure_uploads
-
+from backend import db
 
 app = Flask(__name__)
-
+fileuploads = UploadSet('fileuploads')
+configure_uploads(app, fileuploads)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 @app.route('/upload',methods=["GET",'POST'])
 def upload():
-    return render_template('upload.html')
+    if(request.method=='POST'):
+        filename = fileuploads.save(request.form['fileupload'])
+        url = fileuploads.url(filename)
+        
+    else:
+        return render_template('upload.html')
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
+  app.run(host='127.0.0.1', port=5000, debug=True)
  
--- backend.py
+++ backend.py
@@ -1,7 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
+import os
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
 db = SQLAlchemy(app)
+
+class Misc():
+    genfileid(self):
+        return os.urandom(16).hex()
 
 
 class File(db.Model):
