801f74a09255bfc03572e9b519204f343e5949f4
David J. Fries <friesdj@greenfiber.com>
2019-05-14 15:18:01 -0400

    upload routes

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, flash, request, redirect, url_for,render_template
-import flask-uploads
+from flask_uploads import UploadSet, IMAGES, configure_uploads
 
-@route('/',methods = ['GET'])
-def default():
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
     return render_template('index.html')
+@app.route('/upload',methods=["GET",'POST'])
+def upload():
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port=8000, debug=True)
+ 
